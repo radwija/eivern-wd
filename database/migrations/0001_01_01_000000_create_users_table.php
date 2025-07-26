8f0530879b8f49584d440c0d17048d5219d58f4d
nekoLin <krusiawanbudi1@gmail.com>
2025-07-26 19:41:42 +0800

    Revert "Feature/basic logic"

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Enums\UserRole;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,11 +14,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->enum('role', [
-                UserRole::STUDENT->value,
-                UserRole::ADMIN->value,
-                UserRole::ORGANIZATION_UKM->value,
-            ]);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
