565c7ca16e0acfa7ad3d5b5d0bcc5fa8b4a461cc
radwija <115165737+radwija@users.noreply.github.com>
2025-07-26 10:54:43 +0800

    feat: add thread relation in user

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,4 +56,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Message::class);
     }
+
+    public function threads(): HasMany
+    {
+        return $this->hasMany(Thread::class);
+    }
 }
