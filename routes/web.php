19402b19f83773a0dc03f79b9d70fd33a9743593
nekoLin <krusiawanbudi1@gmail.com>
2025-07-26 14:46:43 +0800

    feat: define routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\ThreadController;
+use App\Models\Event;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,6 +15,15 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+
+    Route::resource('events', EventController::class);
+    Route::resource('comments', CommentController::class)->only([
+        'store', 'destroy',
+    ]);
+    Route::resource('threads', ThreadController::class)->except([
+        'show'
+    ]);
+
 });
 
 Route::get('lost-items/create', function () {
