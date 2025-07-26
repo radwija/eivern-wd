df92a8ee8e6a8e0d2100c8ec122392066415facc
fiksHOKI <samyawiryasuta235@gmail.com>
2025-07-26 11:16:46 +0800

    feat/create-form-for-events-study-lost-socials

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,21 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
+Route::get('lost-items/create', function () {
+    return Inertia::render('lost-items/create');
+})->name('lost-items.create');
+
+Route::get('events/create', function () {
+    return Inertia::render('events/create');
+})->name('events.create');
+
+Route::get('studies/create', function () {
+    return Inertia::render('studies/create');
+})->name('studies.create');
+
+Route::get('socials', function () {
+    return Inertia::render('socials/index');
+})->name('socials.index');
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
